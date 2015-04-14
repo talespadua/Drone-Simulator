fd0bbfcc9a4d59c1fe6302d0b9df445b90ca6f0a
talespadua <talescpadua@gmail.com>
2015-04-14 17:58:01 -0300

    Tratamento do mapa feito. Criado modulo Map. Codigo roda em metodo main

---
+++ Map.py
@@ -0,0 +1,45 @@
+__author__ = 'tales.cpadua'
+import numpy as np
+
+class Map:
+    def __init__(self, soup):
+        #Loading X3D parameters to python variables
+        self.map_tag = soup.find('elevationgrid')
+        self.map_nodes = self.map_tag['height'] # y pointss
+        self.x_size = int(self.map_tag['xdimension'])
+        self.z_size = int(self.map_tag['zdimension'])
+
+        self.map_array = np.zeros((self.x_size, self.z_size)) #matrix of y values. Indexes are X and Y values
+
+        print("Map x value is: " + str(self.x_size))
+        print("Map z value is: " + str(self.z_size))
+        temp = ""
+        i = 0
+        j = 0
+
+        #Filling X3D map to matrix
+        for c in self.map_nodes:
+            if c is '\n':
+                self.map_array[i][j] = int(temp)
+                temp = ""
+                i = i+1
+                j = 0
+                continue
+            if c is ' ':
+                if temp is "":
+                    continue
+                if temp == 'XX':
+                    temp = "02"
+                self.map_array[i][j] = int(temp)
+                temp = ""
+                j = j+1
+                continue
+
+            temp = temp+c
+        print("Map load successful")
+
+    def print_map(self):
+        print("Matrix is: ")
+        for x in range(self.x_size):
+            for z in range(self.z_size):
+                print(self.map_array[x][z])
--- servidor/udp_server.py
+++ servidor/udp_server.py
@@ -1,60 +1,80 @@
 import socket
 import sys
 import configparser
-import numpy as np
 from bs4 import BeautifulSoup
+from Map import Map
 
-config = configparser.RawConfigParser()
-config.read('settings.cfg')
+#Setting up config parser
+def get_config(config_file):
+    config = configparser.RawConfigParser()
+    config.read(config_file)
+    return config
 
-HOST = config.get('settings', 'host')   # Symbolic name meaning all available interfaces
-PORT = int(config.get('settings', 'port')) # Arbitrary non-privileged port
-#
-# with open('../mapas/DotaMap.xml', 'r') as map:
-try:
-    soup = BeautifulSoup(open('../mapas/DotaMap.xml'))
-except:
-    print("erro")
+#Load X3D map to soup
+def load_to_soup(map_path):
+    try:
+        soup = BeautifulSoup(open(map_path))
+    except:
+        print("Error loading map")
+        sys.exit()
+    return soup
 
+def create_socket():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print('Socket created')
+        return s
+    except socket.error as msg:
+        print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
 
-print(soup)
-# Datagram (udp) socket
-try :
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    print('Socket created')
-except socket.error as msg:
-    print('Failed to create socket. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
- 
- 
-# Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
-     
-print('Socket bind complete')
- 
-#now keep talking with the client
-while 1:
-    # receive data from client (data, addr)
-    d = s.recvfrom(512)
-    data = d[0].decode('utf-8')
-    addr = d[1]
+def bind_socket(socket, HOST, PORT):
+    # Bind socket to local host and port
+    try:
+        socket.bind((HOST, PORT))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+    print('Socket bind complete')
 
-    if not data:
+#keep talking with the client
+def begin_listening(socket, PORT):
+    print("Server is listening on port " + PORT.__str__() + "...")
+    while 1:
+        # receive data from client (data, addr)
+        d = socket.recvfrom(512)
+        data = d[0].decode('utf-8')
+        addr = d[1]
+
+        if not data:
+            print("Size of message is: " + str(sys.getsizeof(data)))
+            print("No data received for package in address " + str(addr))
+            socket.sendto(bytes("No data received", encoding="UTF-8"), addr)
+            continue
+
+        reply = 'OK...' + data[1:]
+
+        print("payload was " + str(d))
         print("Size of message is: " + str(sys.getsizeof(data)))
-        print("No data received for package in address " + str(addr))
-        s.sendto(bytes("No data received", encoding="UTF-8"), addr)
-        continue
-     
-    reply = 'OK...' + data[1:]
 
-    print("payload was " + str(d))
-    print("Size of message is: " + str(sys.getsizeof(data)))
+        socket.sendto(bytes(reply, encoding="UTF-8"), addr)
+        print('Message [' + addr[0] + ':' + str(addr[1])[1:] + '] - ' + data[1:].strip())
 
-    s.sendto(bytes(reply, encoding="UTF-8"), addr)
-    print('Message [' + addr[0] + ':' + str(addr[1])[1:] + '] - ' + data[1:].strip())
-     
-s.close()
+
+def main():
+    config = get_config('settings.cfg')
+    soup = load_to_soup('../mapas/DotaMap.xml')
+    map = Map(soup)
+
+    # Datagram (udp) socket
+    HOST = config.get('settings', 'host')   # Symbolic name meaning all available interfaces
+    PORT = int(config.get('settings', 'port')) # Arbitrary non-privileged port
+
+    s = create_socket()
+    bind_socket(s, HOST, PORT)
+
+    begin_listening(s, PORT)
+    s.close()
+
+if __name__ == "__main__":
+    main()
